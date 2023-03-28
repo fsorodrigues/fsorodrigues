5620619e8db04995d647b5a613d05d12a3d4626c
Felippe Rodrigues <fsorodrigues@gmail.com>
2023-03-28 22:18:03 +1300

    New version. updated with 2023 stuff

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,10 +11,10 @@ class Main extends Component {
         return(
             <div className={"main-container"}>
                 <p className={"mb-4"}>
-                    {"Hi! I'm "}<strong>{"Felippe Rodrigues"}</strong>{". I use "}<code className={"p-1 rounded-lg"}>{"data"}</code>{" and "}<code className={"p-1 rounded-lg"}>{"code"}</code>{" to make journalism."}
+                    {"Hi! I'm "}<strong>{"Felippe Rodrigues"}</strong>{". I use "}<code className={"p-1 rounded-1"}>{"data"}</code>{" and "}<code className={"p-1 rounded-1"}>{"code"}</code>{" to make journalism."}
                 </p>
                 <p className={"mb-4"}>
-                    {"I currently work at "}<a href={"https://www.vtdigger.org"}>{"VTDigger.org"}</a>{" as a visual journalist. I'm a front-end developer and data geek too. My reporting and graphics can also be seen on "}<a href={"https://www.politifact.com"}>{"PolitiFact"}</a>, <a href={"https://www.insideclimatenews.org/"}>{"InsideClimate News"}</a>, <a href={"https://www.smithsonianmag.com/"}>{"Smithsonian Mag"}</a>{", among others."}
+                    {"I currently work at "}<a href={"https://stuff.co.nz"}>{"Stuff.co.nz"}</a>{" as a data journalist and web developer. I'm a full-stack developer and data geek too. My reporting and graphics can also be seen on "}<a href={"https://vtdigger.org"}>{"VTDigger.org"}</a>, <a href={"https://www.politifact.com"}>{"PolitiFact"}</a>, <a href={"https://www.insideclimatenews.org/"}>{"InsideClimate News"}</a>, <a href={"https://www.smithsonianmag.com/"}>{"Smithsonian Mag"}</a>{", among others."}
                 </p>
             </div>
         );
--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 // import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
-// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons'
 
 class PortfolioItem extends Component {
     constructor(props) {
@@ -11,14 +12,14 @@ class PortfolioItem extends Component {
     renderTags() {
         return this.props.data.tags.split('||').map(d => {
             return (
-                <code key={d} className={"p-1 rounded-lg ms-1 tag"}>{d}</code>
+                <code key={d} className={"p-1 rounded-1 tag"}>{d}</code>
             );
         });
     }
 
     render() {
         const imgStyle = {
-            background:`url(./media/${this.props.data.slug}.png)`,
+            background:`url(./media/${this.props.data.slug}.png), url(./media/${this.props.data.slug}.jpg)`,
             backgroundSize:"cover"
         };
 
@@ -28,17 +29,23 @@ class PortfolioItem extends Component {
             <div className={"d-flex portfolio-item mb-4"}>
                 <a href={url} target="_blank">
                     <div className={"thumb-container mr-3"}>
-                        <div className={"img-container rounded-lg"} style={imgStyle}></div>
+                        <div className={"img-container rounded-3"} style={imgStyle}></div>
                     </div>
                 </a>
                 <div>
-                    <h6 className="badge badge-custom">{this.props.data.publication}</h6>
+                    <h6 className="badge rounded-pill badge-custom">{this.props.data.publication}</h6>
                     <a href={url}>
                         <h6>{this.props.data.title}</h6>
                     </a>
                     <p>{this.props.data.description}</p>
-                    <div className={"mb-4"}>
+                    <div className={"mb-4 d-inline-flex flex-wrap tagline"}>
                         {this.renderTags()}
+                        {
+                            this.props.data.awards && 
+                            <code className="p-1 rounded-1 tag tag-reverse">
+                                <FontAwesomeIcon icon={faTrophy} /> {this.props.data.awards}
+                            </code>
+                        }
                     </div>
                 </div>
             </div>
